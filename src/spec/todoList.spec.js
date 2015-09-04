34058a3e54300a659232ca2ea468a1111b480655
Tomas Weiss <tomasw@salsitasoft.com>
2015-09-04 11:12:20 +0200

    tests fixed for master reducer

--- src/spec/todoList.spec.js
+++ src/spec/todoList.spec.js
@@ -1,10 +1,15 @@
 import { fromJS } from 'immutable';
+import expect from 'expect';
 
-import { addTodo } from '../client/actions/TodoActions';
-import todoListReducer from '../client/reducers/TodoListReducer';
+import * as TodoActions from '../client/actions/TodoActions';
+import * as TodoListReducer from '../client/reducers/todoListReducer';
+import masterReducer from '../client/reducers/masterReducer';
 
-const expect = require('expect');
+const TESTING_TODO = 'test';
 
+// It's pretty easy to write either integration or unit tests.
+// Because we have master reducer we can simply do the integration test by calling it.
+// All other reducer's functions can be tested in isolation as the unit test.
 describe('Todo list', () => {
   let reduction;
 
@@ -18,11 +23,21 @@ describe('Todo list', () => {
     });
   });
 
-  it('should toggle loader and fire API call to store todo after adding todo', () => {
-    const addTodoAction = addTodo('test');
-    reduction = todoListReducer(reduction, addTodoAction);
+  // This is obviously an integration test beacuse it's testing entire masterReducer which may call multiple reducers functions.
+  it('should toggle loader and fire API call to store todo after adding todo - integration test', () => {
+    const addTodoAction = TodoActions.addTodo(TESTING_TODO);
+    reduction = masterReducer(reduction, addTodoAction);
 
     expect(reduction.getIn(['appState', 'loading'])).toBe(true);
     expect(reduction.getIn(['effects']).count()).toBe(1); // This should be more sophisticated.
+  });
+
+  // This is unit test as it's testing just the single pure todoAdded function.
+  it('should reset loader and contain new todo - unit test', () => {
+    const todoAddedAction = TodoActions.todoAdded(TESTING_TODO);
+    reduction = TodoListReducer.todoAdded(reduction, todoAddedAction.payload);
+
+    expect(reduction.getIn(['appState', 'loading'])).toBe(false);
+    expect(reduction.getIn(['appState', 'todos', 0])).toBe(TESTING_TODO);
   });
 });
